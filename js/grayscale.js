ef64a2dbe734b91feebb1f6f4b4e0043d97d9308
mattiadestefani <mattia.destefani92@gmail.com>
2016-04-01 13:00:28 +0200

    rimesso tutto

---
+++ js/grayscale.js
@@ -0,0 +1,51 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top >20) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $("#navbarsearch").show();
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $("#navbarsearch").hide();
+        $("#searchbar").show();
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
+
+
+
+ 
+    $('#btnDownArrow').hover(function(){
+       $(this).addClass('animated pulse');
+    });
+
+  $(window).ready(function(){
+    $('#first-text').addClass("animated flipInX");
+  });
+
+
